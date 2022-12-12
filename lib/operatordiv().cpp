5212cdda52c08e9ef250f148277dd686129515ea
GeorgeYue1 <115485493+GeorgeYue1@users.noreply.github.com>
2022-12-12 12:04:41 -0600

    Delete operatordiv().cpp

--- lib/operatordiv().cpp
+++
@@ -1,9 +0,0 @@
-#include <cmath>
-#include "../include/Complex.h"
-
-Complex Complex::operator/(Complex com) {
-  Complex c; 
-  c.real = (this->real*com.real + this->imaginary*com.imaginary)/(com.real*com.real + com.imaginary*com.imaginary);
-  c.imaginary = (this->imaginary*com.real - this->real*com.imaginary)/(com.real*com.real + com.imaginary*com.imaginary);
-  return c;
-}
