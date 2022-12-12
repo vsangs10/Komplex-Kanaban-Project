a83ef1d99323d29e1c7f53a20b6755024f1a1766
GeorgeYue1 <115485493+GeorgeYue1@users.noreply.github.com>
2022-12-12 12:04:25 -0600

    Delete operator+().cpp

--- lib/operator+().cpp
+++
@@ -1,9 +0,0 @@
-#include <cmath>
-#include "../include/Complex.h"
-
-Complex Complex::operator+(Complex com) {
-  Complex c; 
-  c.real = this->real + com.real;
-  c.imaginary = this->imaginary + com.imaginary;
-  return c;
-}
