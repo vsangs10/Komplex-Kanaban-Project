ba2ecb350c91a44d2a215e412aa34cad723af6aa
P2 GershonTed <98845943+tedgershon@users.noreply.github.com>
2022-12-12 17:31:01 +0000

    finished pow methods, checked all

--- .ccls-cache/@home@runner@KomplexKanban-v2-P2GershonTed/lib@sqrt().cpp
+++ .ccls-cache/@home@runner@KomplexKanban-v2-P2GershonTed/lib@sqrt().cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 
-Complex sqrt(const Complex c){
-  Complex newC(pow(c.real, 0.5), pow(c.imag, 0.5));
+//Returns the square root of its argument
+Complex sqrt(const Complex c) {
+  Complex newC(pow(abs(c), 0.5)*cos(0.5*arg(c)), pow(abs(c), 0.5)*sin(0.5*arg(c)));
   return newC;
 }
