04b1d58ba6d5f1676c5f1b03fe9f925a403b8b41
tedgershon <98845943+tedgershon@users.noreply.github.com>
2022-12-12 11:45:02 -0600

    Update pow().cpp

--- lib/pow().cpp
+++ lib/pow().cpp
@@ -1,24 +1,22 @@
-#include <cmath>
 #include "../include/Complex.h"
+#include <cmath>
 
+//Takes two arguments: pow(b, exp). It raises b to the power of exp
+//https://www.math.toronto.edu/mathnet/questionCorner/complexexp.html
 Complex pow(double b, const Complex exp) {
-  Complex newC(pow(b, exp.real)*cos(c.imag), pow(b, exp.real)*sin(c.imag));
+  Complex newC(pow(b, real(exp))*cos(imag(exp)*log(b)), pow(b, real(exp))*sin(imag(exp)*log(b)));
   return newC;
 }
 Complex pow(const Complex b, int exp) {
-  Complex newC(pow(b.real, exp), pow(b.imag, exp));
+  Complex newC(pow(abs(c), exp)*cos(exp*arg(c)), pow(abs(c), exp)*sin(exp*arg(c)));
   return newC;
 }
 Complex pow(const Complex b, double exp) {
-  Complex newC(pow(b.real, exp), pow(b.imag, exp));
+  Complex newC(pow(abs(c), exp)*cos(exp*arg(c)), pow(abs(c), exp)*sin(exp*arg(c)));
   return newC;
 }
 Complex pow(const Complex b, const Complex exp) {
-  
+  Complex preEuler(real(exp)*log(abs(b))-imag(exp)*arg(b), imag(exp)*log(abs(b))+real(exp)*arg(b));
+  Complex postEuler = exp(preEuler);
+  return postEuler;
 }
-/* double re = c.real;
-  double im = c.imag;
-  Complex newC;
-  newC.real = pow(EULER, re)*cos(im);
-  newC.imag = pow(EULER, re)*sin(im);
-  return newC; */
