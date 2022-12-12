f9c26673628d3bf32acabe2ff666a2a927b76acb
tedgershon <98845943+tedgershon@users.noreply.github.com>
2022-12-12 11:43:51 -0600

    Update norm().cpp

--- lib/norm().cpp
+++ lib/norm().cpp
@@ -1,8 +1,8 @@
-#include <cmath>
 #include "../include/Complex.h"
+#include <cmath>
 
 double norm(const Complex c) {
   //magSqr is the magnitude squared (a^2+b^2)
-  double magSqr = pow(c.real,2)+pow(c.imag,2);
+  double magSqr = pow(real(c),2)+pow(imag(c),2);
   return magSqr;
 }
