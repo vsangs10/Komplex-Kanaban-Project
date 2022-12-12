e113aec2dd8450c567df6aaa8268616ff6265048
GeorgeYue1 <115485493+GeorgeYue1@users.noreply.github.com>
2022-12-12 12:04:15 -0600

    Delete operator*().cpp

--- lib/operator*().cpp
+++
@@ -1,9 +0,0 @@
-#include <cmath>
-#include "../include/Complex.h"
-
-Complex Complex::operator*(Complex com) {
-  Complex c; 
-  c.real = this->real*com.real - this->imaginary*com.imaginary;
-  c.imaginary = this->real*com.imaginary + this->imaginary*com.real;
-  return c;
-}
